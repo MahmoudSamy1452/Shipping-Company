0048ea078692615db663a063630643f1f3d78140
IslamAmr120 <islam.mohamed02@eng-st.cu.edu.eg>
2022-04-13 22:22:41 +0200

    Queue and priority queue ADTs and implementations.

---
+++ PNode.h
@@ -0,0 +1,79 @@
+
+#ifndef _NODE
+#define _NODE
+template < typename T>
+class PNode
+{
+private:
+	T item; // A data item
+	PNode<T>* next; // Pointer to next node
+	int priority;
+public:
+	PNode();
+	PNode(const T& r_Item, const int& prio);
+	PNode(const T& r_Item, Node<T>* nextNodePtr, const int& prio);
+	void setItem(const T& r_Item);
+	void setNext(PNode<T>* nextNodePtr);
+	void setPriority(const int& prio);
+	T getItem() const;
+	PNode<T>* getNext() const;
+	int getPriority() const;
+}; // end Node
+#endif
+
+template < typename T>
+PNode<T>::PNode() : priority(0)
+{
+	next = nullptr;
+}
+
+template < typename T>
+PNode<T>::PNode(const T& r_Item)
+{
+	item = r_Item;
+	next = nullptr;
+	priority = prio;
+}
+
+template < typename T>
+PNode<T>::PNode(const T& r_Item, PNode<T>* nextNodePtr)
+{
+	item = r_Item;
+	next = nextNodePtr;
+	priority = prio;
+}
+template < typename T>
+void PNode<T>::setItem(const T& r_Item)
+{
+	item = r_Item;
+}
+
+template < typename T>
+void PNode<T>::setNext(PNode<T>* nextNodePtr)
+{
+	next = nextNodePtr;
+}
+
+template < typename T>
+void PNode<T>::setPriority(const int& prio)
+{
+	priority = prio;
+}
+
+template < typename T>
+T PNode<T>::getItem() const
+{
+	return item;
+}
+
+template < typename T>
+PNode<T>* PNode<T>::getNext() const
+{
+	return next;
+}
+
+template < typename T>
+int PNode<T>::getPriority() const
+{
+	return priority;
+}
